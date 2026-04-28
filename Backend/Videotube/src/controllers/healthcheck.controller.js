ffd63deff33e1c2ec4e5848b2f3e847f7fff5405
Gopal <gshahni57@gmail.com>
2026-04-28 22:45:41 +0530

    Revert "pushing the local repo to github at Day 65 , finished Monog Aggregation pipeline"
    This reverts commit acfe27a7401ecfa9c56aec37a130c1eb5ff50aea.

--- Backend/Videotube/src/controllers/healthcheck.controller.js
+++
@@ -1,8 +0,0 @@
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandles.js";
-
-const healthcheck = asyncHandler(async (req, res) => {
-  return res.status(200).json(new ApiRespone(200, "ok", "Healthcheck passed"));
-});
-
-export { healthcheck };
